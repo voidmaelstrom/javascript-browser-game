b71f9f3193a0a9fffcf54619821cc44fead895e5
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-01-29 12:49:26 -0700

    Finish basic tutorial with game to reference

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ let config = {
   }
 };
 
+let score = 0;
+
 let game = new Phaser.Game(config);
 
 function preload ()
@@ -69,11 +71,47 @@ function create ()
 
   cursors = this.input.keyboard.createCursorKeys();
 
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+  });
+
+  stars.children.iterate(function (child) {
+
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+  });
+
+  bombs = this.physics.add.group();
+
+  scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
   this.physics.add.collider(player, platforms);
+  this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(bombs, platforms);
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
+  this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
 function update ()
 {
+  // switch(true) {
+  //   case cursors.left.isDown:
+  //     player.setVelocityX(-160);
+  //     player.anims.play('left', true);
+  //     break;
+  //   case cursors.right.isDown:
+  //     player.setVelocityX(160);
+  //     player.anims.play('right', true);
+  //     break;
+  //   case (cursors.up.isDown && player.body.touching.down):
+  //     player.setVelocityY(-330);
+  //     break;
+  //   default:
+  //     player.setVelocityX(0);
+  //     player.anims.play('turn');
+  // }
   if (cursors.left.isDown)
   {
       player.setVelocityX(-160);
@@ -97,4 +135,40 @@ function update ()
   {
       player.setVelocityY(-330);
   }
+}
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score);
+
+    if (stars.countActive(true) === 0)
+    {
+        stars.children.iterate(function (child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+    }
+}
+
+function hitBomb (player, bomb)
+{
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    gameOver = true;
 }
